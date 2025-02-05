49f932da71a0a802339d779eea1a9241384cab79
hayathdt <hayat.haddouti2@gmail.com>
2025-02-05 13:08:28 +0100

    création du modèle Task avec schéma pour les tâches + maj de mongoose

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import Task from "./models/task.js";
+
 const app = express();
-require("dotenv").config();
-const mongoose = require("mongoose");
+
+import dotenv from "dotenv";
+dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connexion à MongoDB réussie"))
   .catch((err) => console.log("Erreur de connexion à MongoDB", err));
 
-const PORT = 8000;
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = new Task({
+      title: req.body.title,
+      description: req.body.description,
+    });
+    const savedTask = await task.save();
+    res.status(200).json(savedTask);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Bienvenue");
